0299c1f39dea0efcf171323605535dc8b4e818cb
Kyosuke YOSHIMURA <kyosuke.yoshimura@gmail.com>
2013-05-13 22:12:34 +0900

    PhoneGap 2.7.0対応

--- Android/Analytics/2.0/src/com/phonegap/plugins/analytics/GoogleAnalyticsTracker.java
+++ Android/Analytics/2.0/src/com/phonegap/plugins/analytics/GoogleAnalyticsTracker.java
@@ -2,83 +2,130 @@
  * PhoneGap is available under *either* the terms of the modified BSD license *or* the
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  *
- * Copyright (c) 2006-2011 Worklight, Ltd.  
+ * Copyright (c) 2006-2011 Worklight, Ltd.
+ * Upgraded by Doers' Guild
  */
 
 package com.phonegap.plugins.analytics;
 
-import org.apache.cordova.api.Plugin;
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
 import org.apache.cordova.api.PluginResult;
-import org.apache.cordova.api.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.util.Log;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.GAServiceManager;
+import com.google.analytics.tracking.android.Tracker;
 
-public class GoogleAnalyticsTracker extends Plugin {
-	public static final String START = "start";
-	public static final String TRACK_PAGE_VIEW = "trackPageView";
-	public static final String TRACK_EVENT = "trackEvent";
-	public static final String SET_CUSTOM_VARIABLE = "setCustomVariable";
-    
-	public static final int DISPATCH_INTERVAL = 20;
-	private com.google.android.apps.analytics.GoogleAnalyticsTracker tracker;
-	
-	public GoogleAnalyticsTracker() {
-		tracker = com.google.android.apps.analytics.GoogleAnalyticsTracker.getInstance();
-	}
-	
-	@Override
-	public PluginResult execute(String action, JSONArray data, String callbackId) {
-		PluginResult result = null;
-		if (START.equals(action)) {
-			try {
-				start(data.getString(0));
-				result = new PluginResult(Status.OK);
-			} catch (JSONException e) {
-				result = new PluginResult(Status.JSON_EXCEPTION);
-			}
-		} else if (TRACK_PAGE_VIEW.equals(action)) {
-			try {
-				trackPageView(data.getString(0));
-				result = new PluginResult(Status.OK);
-			} catch (JSONException e) {
-				result = new PluginResult(Status.JSON_EXCEPTION);
-			}
-		} else if (TRACK_EVENT.equals(action)) {
-			try {
-				trackEvent(data.getString(0), data.getString(1), data.getString(2), data.getInt(3));
-				result = new PluginResult(Status.OK);
-			} catch (JSONException e) {
-				result = new PluginResult(Status.JSON_EXCEPTION);
-			}
-		} else if (SET_CUSTOM_VARIABLE.equals(action)){
-			try {
-				setCustomVar(data.getInt(0), data.getString(1), data.getString(2), data.getInt(3));
-				result = new PluginResult(Status.OK);
-			} catch (JSONException e) {
-				result = new PluginResult(Status.JSON_EXCEPTION);
-			}
-		} else {
-			result = new PluginResult(Status.INVALID_ACTION);
-		}
-		return result;
-	}
-	
-	private void start(String accountId) {
-		tracker.startNewSession(accountId, DISPATCH_INTERVAL, this.cordova.getActivity());
-	}
-	
-	private void trackPageView(String key) {
-		tracker.trackPageView(key);
-	}
+public class GoogleAnalyticsTracker extends CordovaPlugin {
+    public static final String START = "start";
+    public static final String STOP = "stop";
+    public static final String TRACK_PAGE_VIEW = "trackPageView";
+    public static final String TRACK_EVENT = "trackEvent";
+    public static final String SET_CUSTOM_DIMENSION = "setCustomDimension";
+    public static final String TRACK_TIMING = "trackTiming";
 
-	private void trackEvent(String category, String action, String label, int value){
-		tracker.trackEvent(category, action, label, value);
-	}
+    private Tracker tracker;
+    private com.google.analytics.tracking.android.EasyTracker instance;
 
-	private void setCustomVar(int index, String label, String value, int scope) {
-		if(scope > 0) tracker.setCustomVar(index, label, value, scope);
-		else tracker.setCustomVar(index, label, value);
-	}
+    public GoogleAnalyticsTracker() {
+        instance = com.google.analytics.tracking.android.EasyTracker
+                .getInstance();
+    }
+
+    @Override
+//    public PluginResult execute(String action, JSONArray data, String callbackId) {
+    public boolean execute(String action, JSONArray data, CallbackContext context) {
+        if (START.equals(action)) {
+            this.start();
+            context.success();
+            return true;
+        } else if (TRACK_PAGE_VIEW.equals(action)) {
+            try {
+                this.trackPageView(data.getString(0));
+                context.success();
+                return true;
+            } catch (JSONException e) {
+                this.error(context, PluginResult.Status.JSON_EXCEPTION);
+                return true;
+            }
+        } else if (TRACK_EVENT.equals(action)) {
+            try {
+                this.trackEvent(
+                        data.getString(0),
+                        data.getString(1),
+                        data.getString(2),
+                        data.getLong(3));
+                context.success();
+                return true;
+            } catch (JSONException e) {
+                this.error(context, PluginResult.Status.JSON_EXCEPTION);
+                return true;
+            }
+        } else if (STOP.equals(action)) {
+            this.stop();
+            context.success();
+            return true;
+        } else if (SET_CUSTOM_DIMENSION.equals(action)) {
+            try {
+                setCustomDimension(data.getInt(0), data.getString(1));
+                context.success();
+                return true;
+            } catch (JSONException e) {
+                this.error(context, PluginResult.Status.JSON_EXCEPTION);
+                return true;
+            }
+        } else if (TRACK_TIMING.equals(action)) {
+            try {
+                trackTiming(
+                        data.getString(0),
+                        data.getLong(1),
+                        data.getString(2),
+                        data.getString(3));
+                context.success();
+                return true;
+            } catch (JSONException e) {
+                this.error(context, PluginResult.Status.JSON_EXCEPTION);
+                return true;
+            }
+        } else {
+            // falseを返すと、this.error(context, Status.INVALID_ACTION)を内部でやってくれてるっぽい。
+            return false;
+        }
+    }
+
+    private void start() {
+        instance.activityStart(this.cordova.getActivity());
+        tracker = EasyTracker.getTracker();
+    }
+
+    private void stop() {
+        instance.activityStop(this.cordova.getActivity());
+    }
+
+    private void trackPageView(String key) {
+        tracker.sendView(key);
+        GAServiceManager.getInstance().dispatch();
+    }
+
+    private void trackEvent(String category, String action, String label,
+            long value) {
+        tracker.sendEvent(category, action, label, value);
+        GAServiceManager.getInstance().dispatch();
+    }
+
+    private void trackTiming(String category, long loadTime, String name,
+            String label) {
+        tracker.sendTiming(category, loadTime, name, label);
+    }
+
+    private void setCustomDimension(int Index, String dimensionValue) {
+        tracker.setCustomDimension(Index, dimensionValue);
+    }
+
+    // CallbackContext#errorの自前ラッパー
+    private void error(CallbackContext context, PluginResult.Status status) {
+        context.error(PluginResult.StatusMessages[status.ordinal()]);
+    }
 }
